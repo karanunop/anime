b753225bc220f6994d31504449ea74063c49fd97
karanunop <114085109+karanunop@users.noreply.github.com>
2024-08-14 13:03:28 -0300

    fix prisma

---
+++ prisma/migrations/20240814160247_initial_migration/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE "Sign" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Sign_email_key" ON "Sign"("email");
